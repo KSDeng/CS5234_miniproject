8a35e0e59c4ee39a7ad48758704346b1c88f19e3
Endjy <39741444+Endjy@users.noreply.github.com>
2021-11-03 15:49:00 +0800

    Add files via upload

--- graph_generator.py
+++ graph_generator.py
@@ -4,17 +4,26 @@ from networkx.generators.random_graphs import erdos_renyi_graph
 import networkx as nx
 import matplotlib.pyplot as plt
 
-n = 100
-p = 0.05
+n = 6
+p = 0.5
 #g = nx.generators.random_graphs.random_regular_graph(n, p)
 g = erdos_renyi_graph(n, p)
+pos = nx.spring_layout(g)
+node1=0
+node2=1
+i=0
+path_edges=list(zip([node1],[node2]))
 
 print(g.nodes)
-# [0, 1, 2, 3, 4, 5]
-
 print(g.edges)
-# [(0, 1), (0, 2), (0, 4), (1, 2), (1, 5), (3, 4), (4, 5)]
 
+nx.draw(g,pos,with_labels = True)
+nx.draw_networkx_edges(g,pos,edgelist=path_edges,edge_color='r')
+plt.savefig("video_graph"+str(i)+ ".png")
+i=1
+node1=2
+node2=3
 
-nx.draw(g, with_labels = True)
-plt.savefig("filename.png")
+path_edges=list(zip([node1],[node2]))
+nx.draw_networkx_edges(g,pos,edgelist=path_edges,edge_color='r')
+plt.savefig("video_graph"+str(i)+ ".png")
